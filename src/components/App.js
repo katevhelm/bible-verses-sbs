ca58f94ed2a52ab09e2621b7a0bdfc04c04af64a
Kate Helm <katevhelm@gmail.com>
2022-04-13 15:52:13 -0400

    create and link VerseSelect

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import VerseSelect from './VerseSelect';
 
 class App extends React.Component {
   state = {};
 
   render() {
-    return <div>Bible Verses Side by Side</div>;
+    return (
+      <div>
+        <h1>Bible Verses Side by Side</h1>
+        <VerseSelect />
+      </div>
+    );
   }
 }
 
---
+++ src/components/VerseSelect.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const VerseSelect = (props) => {
+  return (
+    <div>
+      <p>Choose a verse:</p>
+      <form>
+        <label for="books">Books</label>
+        <select name="books" id="books">
+          <option value="genesis">Genesis</option>
+          <option value="exodus">Exodus</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default VerseSelect;
